4799a51df4897aea452f891b2c5b798fb4c2ce63
vanchimca <vanchi.nat@gmail.com>
2022-02-16 22:55:31 +0530

    First Commit

---
+++ src/test/java/com/auction/server/AuctionserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auction.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
